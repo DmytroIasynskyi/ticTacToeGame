4378cc14b93b692dae03d0ab0b660ca7343ef671
DmytroIasynskyi <dmytro.iasynskyi@vitechteam.com>
2024-01-10 17:59:03 +0200

    game implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import {useEffect, useState} from 'react'
 
-function App() {
+function Board() {
+  const initialSquares = Array(9).fill(null);
+  const key = 'squares';
+  const [squares, setSquares] = useState(() => {
+    const valueInStorage = window.localStorage.getItem(key);
+    if (typeof valueInStorage === 'string') return JSON.parse(window.localStorage.getItem(key));
+    return initialSquares;
+  });
+
+  const nextValue = calculateNextValue(squares);
+  const winner = calculateWinner(squares);
+  const status = calculateStatus(winner, squares, nextValue);
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(squares))
+  }, [squares])
+
+  function selectSquare(square) {
+    if (winner || squares[square]) return;
+
+    const squaresCopy = [...squares]
+    squaresCopy[square] = nextValue;
+    setSquares(squaresCopy);
+  }
+
+  function restart() {
+    setSquares(initialSquares)
+  }
+
+  function renderSquare(i) {
+    return (
+        <button className="square" onClick={() => selectSquare(i)}>
+          {squares[i]}
+        </button>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+  )
 }
 
-export default App;
+function Game() {
+  return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+  )
+}
+
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+      ? `Winner: ${winner}`
+      : squares.every(Boolean)
+          ? `Scratch: Cat's game`
+          : `Next player: ${nextValue}`
+}
+
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+function App() {
+  return <Game />
+}
+
+export default App
